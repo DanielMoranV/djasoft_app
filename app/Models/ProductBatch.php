5c8ca229c6cd7b966ee5a47204bcf1e6d47b0965
Daniel Moran Vilchez <113327439+DanielMoranV@users.noreply.github.com>
2024-09-02 06:38:37 -0500

    Implementacion de añadir movimientos de stock

--- app/Models/ProductBatch.php
+++ app/Models/ProductBatch.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+use Exception;
 
 class ProductBatch extends Model
 {
@@ -13,8 +16,35 @@ class ProductBatch extends Model
     protected $table = 'product_batches';
 
     protected $fillable = [
-        'product_id', 'batch_number', 'expiration_date', 'price', 'quantity',
+        'product_id',
+        'batch_number',
+        'expiration_date',
+        'price',
+        'quantity',
     ];
+
+    protected function expirationDate(): Attribute
+    {
+        return Attribute::make(
+            // Accesor: Formatea la fecha cuando se obtiene de la base de datos.
+            get: function ($value) {
+                try {
+                    return Carbon::parse($value)->format('Y-m-d');
+                } catch (Exception $e) {
+                    // Manejo de errores, por ejemplo, si el valor es nulo o inválido
+                    return $value; // Devolver el valor sin cambios si ocurre un error
+                }
+            },
+            // Mutador: Convierte la fecha al formato Y-m-d antes de guardarla.
+            set: function ($value) {
+                try {
+                    return Carbon::parse($value)->format('Y-m-d');
+                } catch (Exception $e) {
+                    return null; // Manejo de errores si la fecha no es válida
+                }
+            }
+        );
+    }
 
     public function product()
     {
